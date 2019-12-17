f437847ed44ff17ae11d749b33d477b84177782e
Pame1995 <pamela_pachacama@hotmail.com>
2019-12-17 09:03:03 -0500

    iloc-loc

---
+++ 02-pandas/G_iloc_loc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 08:04:33 2019
+
+@author: USRBET
+"""
+
+import pandas as pd
+
+path_guardado_completo = "C://Users//USRBET//Documents//GitHub//py-pachacama-simba-a-pamela-abigail//02-pandas//data//artwork_data_completo.pickle"
+df = pd.read_pickle(path_guardado_completo)
