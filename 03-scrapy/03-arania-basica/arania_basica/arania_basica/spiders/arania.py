eb98dc74322cc666dbd56de961c7a6e92898686b
Pame1995 <pamela_pachacama@hotmail.com>
2020-01-18 00:08:46 -0500

    deber-scrapy

--- 03-scrapy/03-arania-basica/arania_basica/arania_basica/spiders/arania.py
+++ 03-scrapy/03-arania-basica/arania_basica/arania_basica/spiders/arania.py
@@ -1,27 +1,66 @@
 import scrapy
 
+def guardar_datos(titulo, precio, imagen):
+    import csv
+    with open('datos.csv','ab',) as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+
+        data = list(zip(titulo, precio, imagen))
+        for row in data:
+            row = list(row)
+            spamwriter.writerow(row)
+    print("creado")
+
 class IntroSpider(scrapy.Spider):
     name = 'introduccion_spider' 
-    urls = ['http://books.toscrape.com/catalogue/category/books/travel_2/index.html']
+    urls = ['http://books.toscrape.com/index.html']
+    lista_categorias=[]
+    bandera = False
 
     def start_requests(self):
         for url in self.urls:
             yield scrapy.Request(url=url)
 
+
     def parse(self, response):
         def precio_f(precio):
             return float(precio[1::])
+        
+        if(self.bandera == False):
+            etiqueta_contenedora= response.css('div.side_categories')
+            categorias =etiqueta_contenedora.css(' ul > li > ul > li > a::attr(href)').extract()
+            print(categorias)
+            self.lista_categorias.append("http://books.toscrape.com/"+ categorias[0])
+            for p in categorias[1:]:
+                p =  "http://books.toscrape.com/"+p
+                self.lista_categorias.append(p)
+            print(self.lista_categorias)
+            for p in self.lista_categorias[1:]:
+                yield scrapy.Request(p, callback=self.parse)
+                self.bandera = True
+        else:
+            etiqueta_contenedora = response.css('article.product_pod')
+            titulos = etiqueta_contenedora.css('h3 > a::text').extract()
+            precios = etiqueta_contenedora.css('div.product_price > p.price_color::text').extract()
+            precio_float= list(map(precio_f, precios))
+            imagenes = etiqueta_contenedora.css('div.image_container > a > img ::attr(src)').extract()
+            lista_imagenes=[]
+            for p in imagenes:
+                p = "http://books.toscrape.com"+p[11:]
+                lista_imagenes.append(p)
+
+            print(titulos)
+            print(lista_imagenes)
+            print(precio_float)
+            guardar_datos(titulos, precio_float, lista_imagenes)
+            
+            
+
+               
 
 
-        #etiqueta_contenedora = response.css('article.product_pod')
-        #titulos = etiqueta_contenedora.css('h3 > a::text').extract()
-        #precios = etiqueta_contenedora.css('div.product_price > p.price_color::text').extract()
-        #precio_float= list(map(precio_f, precios))
-        #imagenes = etiqueta_contenedora.css('div.image_container > a > img ::attr(src)').extract()
-        etiqueta_contenedora2= response.css('div.side_categories')
-        categorias =etiqueta_contenedora2.css(' ul > li > ul > li > a::attr(href)').extract()
 
-        #print(titulos)
-        #print(imagenes)
-        #print(precio_float)
-        print(categorias)
+
+
+     
+
