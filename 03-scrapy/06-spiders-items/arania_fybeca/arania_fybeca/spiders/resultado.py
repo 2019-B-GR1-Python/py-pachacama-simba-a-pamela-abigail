c62096f2b6e70c1ba92fe222b0fad5224b37be2f
Pame1995 <pamela_pachacama@hotmail.com>
2020-02-17 22:40:19 -0500

    deber-funcionando

--- 03-scrapy/06-spiders-items/arania_fybeca/arania_fybeca/spiders/resultado.py
+++ 03-scrapy/06-spiders-items/arania_fybeca/arania_fybeca/spiders/resultado.py
@@ -5,7 +5,7 @@ import os
 import nltk
 import seaborn as sns
 
-path_csv = "C:\\Users\\Ale\\Documents\\GitHub\\py-macas-cevallos-alexandra-vanessa\\04 - scrapy\\06-spider-items\\arania_fybeca\\arania_fybeca\\tmp\\productos-fybeca_final.csv"
+path_csv = "C:\\Users\\Pamela\\Documents\\GitHub\\py-pachacama-simba-a-pamela-abigail\\03-scrapy\\06-spiders-items\\arania_fybeca\\tmp\\productos-fybeca_final.csv"
 data_frame_test = pd.read_csv(path_csv,  encoding = 'unicode_escape',sep = ",")
 data_frame_test.dropna()
 data_frame_test.dtypes
